6ed58205b80785eafc8f7670a95d55a57a13b95c
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-24 16:01:52 +0200

    Completed task number 6

--- tasks/06-data-types/finish.js
+++ tasks/06-data-types/finish.js
@@ -1,14 +1,49 @@
-/** ЗАДАЧА 6 - Типы данных
+/** ЗАВДАННЯ 6 - Типи даних
  *
- * 1. Объявите несколько переменных и присвойте им значения:
- *  - строка
- *  - число
- *  - логическое
- *  - null
- *  - undefined
- *  - объект
- *  - массив
+ * 1. Оголосіть кілька змінних і надайте їм значення:
+ * - рядок
+ * - число
+ * - логічне
+ * - null
+ * - undefined
+ * - об'єкт
+ * - масив
  *
- * 2. Выведите в консоль тип каждого из значений
- * используя оператор typeof
+ * 2. Виведіть у консоль тип кожного з значень
+ * Використовуючи оператор typeof
  */
+
+const greeting = 'Hello';
+
+const totalAmount = 100;
+
+const isEnrolledStudent = true;
+
+const emptyValue = null;
+
+const undefinedValue = undefined;
+
+let myUndefined;
+
+const user = {
+  name: 'Ku Ku',
+  age: 40,
+};
+
+const numbersArray = [1, 2, 3];
+
+console.log(typeof greeting);
+
+console.log(typeof totalAmount);
+
+console.log(typeof isEnrolledStudent);
+
+console.log(typeof emptyValue);
+
+console.log(typeof undefinedValue);
+
+console.log(typeof myUndefined);
+
+console.log(typeof user);
+
+console.log(typeof numbersArray);
