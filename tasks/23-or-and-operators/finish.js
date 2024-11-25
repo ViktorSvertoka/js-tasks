94d6c65810f8da902e9749dbb5289a1bf83b7565
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-25 17:47:00 +0200

    Completed task number 23

--- tasks/23-or-and-operators/finish.js
+++ tasks/23-or-and-operators/finish.js
@@ -1,6 +1,21 @@
-/** ЗАДАЧА 23 - ИЛИ и И операторы
+/** ЗАВДАННЯ 23 - АБО та І оператори
  *
- * Что будет выведено в консоль?
+ * Що буде виведено в консоль?
  */
 
-console.log(3 || (true && null) || false)
+console.log(3 || (true && null) || false); // <-- 3
+
+/** КРОК 1
+ * (true && null)
+ * null
+ */
+
+/** КРОК 2
+ * 3 || null || false
+ * 3
+ */
+
+console.log(true && null && 10); // <-- null
+console.log(true && 'abc' && 10); // <-- 10
+
+console.log(10 || false); // <-- 10
