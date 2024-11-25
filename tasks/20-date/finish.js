d291e66ee5eeb17c5ad7b2ad59e6cb6fc403267e
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-25 11:33:47 +0200

    Completed task number 20

--- tasks/20-date/finish.js
+++ tasks/20-date/finish.js
@@ -1,6 +1,16 @@
-/** ЗАДАЧА 20 - Дата
+/** ЗАВДАННЯ 20 - Дата
  *
- * 1. Создайте переменную и присвойте ей текущее время в миллисекундах
+ * 1. Створіть змінну та надайте їй поточний час у мілісекундах
  *
- * 2. Выведите значение в консоль
+ * 2. Виведіть значення в консоль
  */
+
+const currentDate = new Date().getTime();
+
+console.log(currentDate);
+
+const currentDate2 = new Date();
+
+const currentDateInMs = currentDate2.getTime();
+
+console.log(currentDateInMs);
