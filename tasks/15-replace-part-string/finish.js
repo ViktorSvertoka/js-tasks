e6a38ea8109ba837141d1908b5f1fcebe4e2b420
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-25 10:51:43 +0200

    Completed task number 15

--- tasks/15-replace-part-string/finish.js
+++ tasks/15-replace-part-string/finish.js
@@ -1,8 +1,16 @@
-/** ЗАДАЧА 15 - Замена части строки
+/** ЗАВДАННЯ 15 - Заміна частини рядка
  *
- * 1. Создайте переменную и присвойте ей строку "Good Morning"
+ * 1. Створіть змінну та надайте їй рядок "Good Morning"
  *
- * 2. Замените "Morning" на "Evening" и присвойте результат новой переменной
+ * 2. Замініть "Morning" на "Evening" і надайте результат нової змінної
  *
- * 3. Выведите значение второй переменной в консоль
+ * 3. Виведіть значення другої змінної в консоль
  */
+
+const greeting = 'Good Morning!';
+
+console.log(greeting);
+
+const updatedGreeting = greeting.replace('Morning', 'Evening');
+
+console.log(updatedGreeting);
