82e755570c84181c8a1ddecf176d635fdacc1b61
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-26 19:12:57 +0200

    Completed task number 41

--- tasks/41-find-single-post/finish.js
+++ tasks/41-find-single-post/finish.js
@@ -1,23 +1,27 @@
-/** ЗАДАЧА 41 - Поиск объектов в массиве
+/** ЗАВДАННЯ 41 - Пошук об'єктів у масиві
  *
- * 1. Создайте функцию "findPostById" с двумя параметрами:
- *  - ID поста
- *  - массив постов
+ * 1. Створіть функцію "findPostById" із двома параметрами:
+ * - ID поста
+ * - масив постів
  *
- * 2. Функция должна вернуть пост с определенным ID
+ * 2. Функція повинна повернути пост із певним ID
  *
- * 3. Если поста с определенным ID в массиве постов нет,
- * функция должна вернуть "undefined"
+ * 3. Якщо посту з певним ID у масиві постів немає,
+ * функція має повернути "undefined"
  *
- * 4. Также внутри функции выведите в консоль ID поста
+ * 4. Також всередині функції виведіть у консоль ID посту
  */
+
+function findPostById(postId, posts) {
+  return posts.find(post => post.postId === postId);
+}
 
 const posts = [
   { postId: 1355, commentsQuantity: 5 },
   { postId: 5131, commentsQuantity: 13 },
   { postId: 6134, commentsQuantity: 2 },
-]
+];
 
-console.log(findPostById(6134, posts)) // { postId: 6134, commentsQuantity: 2 }
+console.log(findPostById(6134, posts)); // { postId: 6134, commentsQuantity: 2 }
 
-console.log(findPostById(4511, posts)) // undefined
+console.log(findPostById(4511, posts)); // undefined
