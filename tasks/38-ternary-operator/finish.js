64f9d5a261fae18060f42671c1b9fc7a7cd85e17
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-26 11:03:54 +0200

    Completed task number 38

--- tasks/38-ternary-operator/finish.js
+++ tasks/38-ternary-operator/finish.js
@@ -1,19 +1,23 @@
-/** ЗАДАЧА 38 - Тернарный оператор
+/** ЗАВДАННЯ 38 - Тернарний оператор
  *
- * 1. Перепишите инструкцию "if .. else" без использования "else"
+ * 1. Перепишіть інструкцію "if .. else" без використання "else"
  *
- * 2. Перепишите содержимое функции, используя тернарный оператор
+ * 2. Перепишіть вміст функції за допомогою тернарного оператора
  *
- * 3. Замените обычную функцию на стрелочную функцию
+ * 3. Замініть звичайну функцію на стрілочну функцію
  */
 
-function isArrayEmpty(inputArray) {
-  if (inputArray.length > 0) {
-    return 'Массив не пустой'
-  } else {
-    return 'Массив пустой'
-  }
-}
+// function isArrayEmpty(inputArray) {
+//   if (inputArray.length > 0) {
+//     return 'Масив не порожній';
+//   } else {
+//     return 'Масив порожній';
+//   }
+// }
 
-console.log(isArrayEmpty([1, 3]))
-console.log(isArrayEmpty([]))
+const isArrayEmpty = inputArray => {
+  return inputArray.length > 0 ? 'Масив не порожній' : 'Масив порожній';
+};
+
+console.log(isArrayEmpty([1, 3]));
+console.log(isArrayEmpty([]));
