ba89658aae3e7606df455fd608b4b53b04d4bb7e
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-26 11:41:06 +0200

    Completed task number 39

--- tasks/39-foreach-with-index/finish.js
+++ tasks/39-foreach-with-index/finish.js
@@ -9,4 +9,9 @@
  * и выведите в консоль результат вызова функции
  */
 
-const myCities = ['London', 'New York', 'Singapore']
+const myCities = ['London', 'New York', 'Singapore'];
+
+const cityInfo = (city, index) =>
+  `${city} is at the index ${index} in the myCities.`;
+
+myCities.forEach((city, index) => console.log(cityInfo(city, index)));
