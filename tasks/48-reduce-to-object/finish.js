dafd611775a398dc296695b7faf013f20a5eb5a8
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-26 23:28:12 +0200

    Completed task number 48

--- tasks/48-reduce-to-object/finish.js
+++ tasks/48-reduce-to-object/finish.js
@@ -1,11 +1,21 @@
-/** ЗАДАЧА 48 - Использование метода "reduce" для создания объекта
+/** ЗАВДАННЯ 48 - Використання методу "reduce" для створення об'єкта
  *
- * 1. Создайте функцию "quantitiesByCategories" с одним параметром "products"
+ * 1. Створіть функцію "quantitiesByCategories" з одним параметром "products"
  *
- * 2. Эта функция должна возвращать объект с ключами,
- * равными категориям, и значениями,
- * равными сумме всех количеств в каждой категории
+ * 2. Ця функція повинна повертати об'єкт із ключами,
+ * рівними категоріям, і значеннями,
+ * рівними сумі всіх кількостей у кожній категорії
  */
+
+const quantitiesByCategories = products => {
+  return products.reduce((qtysByCategories, product) => {
+    const { category, quantity } = product;
+
+    qtysByCategories[category] = (qtysByCategories[category] || 0) + quantity;
+
+    return qtysByCategories;
+  }, {});
+};
 
 const inputProducts = [
   {
@@ -32,9 +42,10 @@ const inputProducts = [
     quantity: 2,
     category: 'Watches',
   },
-]
+];
 
-console.log(quantitiesByCategories(inputProducts))
+console.log(quantitiesByCategories(inputProducts));
+
 /* {
   Accessories: 3,
   Phones: 1,
