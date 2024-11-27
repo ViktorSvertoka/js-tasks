1f4b9c20d39a218859c664a9ea230e4dd5e96874
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-27 22:28:29 +0200

    Completed task number 78

--- tasks/78-classes/finish.js
+++ tasks/78-classes/finish.js
@@ -1,22 +1,33 @@
-/** ЗАДАЧА 78 - Классы
+/** ЗАВДАННЯ 78 - Класи
  *
- * 1. Создайте класс "Fruit"
+ * 1. Створіть клас "Fruit"
  *
- * 2. У каждого экземпляра этого класса должно быть
- * два собственных свойства:
- *  - title
- *  - price
+ * 2. У кожного екземпляра цього класу має бути
+ * дві власні характеристики:
+ * - title
+ * - price
  *
- * 3. Также нужно добавить метод "priceInfo", который
- * будет возвращать строку, содержащую название и цену фрукта
+ * 3. Також потрібно додати метод "priceInfo", який
+ * повертатиме рядок, що містить назву та ціну фрукта
  *
- * 4. Выводы в консоли должны совпасть
+ * 4. Висновки в консолі повинні збігтися
  */
 
-const apple = new Fruit('Apple', 2)
-console.log(apple.priceInfo())
+class Fruit {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+
+  priceInfo() {
+    return `Price of the ${this.title} is ${this.price}$`;
+  }
+}
+
+const apple = new Fruit('Apple', 2);
+console.log(apple.priceInfo());
 // Price of the Apple is 2$
 
-const orange = new Fruit('Orange', 3)
-console.log(orange.priceInfo())
+const orange = new Fruit('Orange', 3);
+console.log(orange.priceInfo());
 // Price of the Orange is 3$
