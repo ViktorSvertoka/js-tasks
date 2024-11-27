e0f4321c4afed7eb99090acd7b8dd2b6aef94bc1
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-27 22:45:10 +0200

    Completed task number 80

--- tasks/80-sum-of-positive-and-negative-numbers/finish.js
+++ tasks/80-sum-of-positive-and-negative-numbers/finish.js
@@ -1,20 +1,43 @@
-/** ЗАДАЧА 80 - Сумма положительных и отрицательных чисел
+/** ЗАВДАННЯ 80 - Сума позитивних та негативних чисел
  *
- * 1. Создайте функцию "sumPositiveNegative", которая принимает массив
- * и суммирует отдельно положительные и отрицательные числа.
+ * 1. Створіть функцію "sumPositiveNegative", яка приймає масив
+ * і підсумовує окремо позитивні та негативні числа.
  *
- * 2. Функция должна вернуть такой объект:
- *  {
- *    positive: 74, // сумма всех положительных чисел
- *    negative: -54 // сумма всех отрицательных чисел
- *  }
+ * 2. Функція повинна повернути такий об'єкт:
+ * {
+ * positive: 74, // сума всіх позитивних чисел
+ * negative: -54 // сума всіх негативних чисел
+ * }
  */
 
-const nums = [10, -12, 30, -1, -8, 0, 14, -33, 20]
+const nums = [10, -12, 30, -1, -8, 0, 14, -33, 20];
 
-// Создайте функцию "sumPositiveNegative" здесь
+// Створіть функцію "sumPositiveNegative" тут
 
-const result = sumPositiveNegative(nums)
+function sumPositiveNegative(arr) {
+  return arr.reduce(
+    (sums, num) => {
+      if (num > 0) {
+        return {
+          positive: sums.positive + num,
+          negative: sums.negative,
 
-console.log(result)
-// { positive: 74, negative: -54 }
+          // ...sums,
+          // positive: sums.positive + num,
+        };
+      }
+      return {
+        positive: sums.positive,
+        negative: sums.negative + num,
+
+        // ...sums,
+        // negative: sums.negative + num,
+      };
+    },
+    { positive: 0, negative: 0 },
+  );
+}
+
+const result = sumPositiveNegative(nums);
+
+console.log(result); // { positive: 74, negative: -54 }
