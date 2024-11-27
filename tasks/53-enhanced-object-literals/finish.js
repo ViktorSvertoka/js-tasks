d2c54d4597724e2541846b014f08ed1922591d0f
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-27 14:54:07 +0200

    Completed task number 53

--- tasks/53-enhanced-object-literals/finish.js
+++ tasks/53-enhanced-object-literals/finish.js
@@ -1,54 +1,52 @@
-/** ЗАДАЧА 53 - Сокращенное написание свойств объектов
- * 
- * 1. Исправьте все ошибки в коде.
- * 
- * 2. Измените объект, возвращаемый функцией "photosGallery", используя:
- *  - Сокращенные имена свойств
- *  - Сокращенные имена методов
- *  - Также нужно изменить одно свойство объекта
- * и сделать его вычисляемым свойством
- * 
- * 3. Сообщения в консоли должны быть точно такими же, 
- * как и в конце этой задачи
+/** ЗАВДАННЯ 53 - Скорочене написання властивостей об'єктів
+ *
+ * 1. Виправте всі помилки в коді.
+ *
+ * 2. Змініть об'єкт, що повертається функцією "photosGallery", використовуючи:
+ * - Скорочені імена властивостей
+ * - Скорочені імена методів
+ * - Також потрібно змінити одну властивість об'єкта
+ * і зробити його обчислюваною властивістю
+ *
+ * 3. Повідомлення в консолі повинні бути такими ж,
+ * як і в кінці цього завдання
  */
 
 const photosGallery = (title, dimensions, date) => {
   return {
-    title: title,
-    info: function() {
+    date,
+    title,
+    [dimensions]: true,
+
+    info() {
+      console.log(`Фото "${title}" має дозвіл ${dimensions}`);
+    },
+
+    publishInfo() {
       console.log(
-        "Фото "${title}" имеет разрешение ${date}`
+        `Фото "${title}" було опубліковано ${Math.floor(
+          (new Date().getTime() - date.getTime()) / 1000,
+        )} секунди тому`,
       );
     },
-    dimensions: dimensions
-    publishInfo: () => {
-      console.log(
-        `Фото было опубликовано ${Math.floor(
-          (new Date().getTime() - date.getTime()) / 1000
-        )} секунды назад`
-      );
-    ,
-    date: date
-  }
-}
+  };
+};
 
-const myDogPhoto = photosGallery(
-  "My dog",
-  "1920x1080",
-  new Date()
-)
+const myDogPhoto = photosGallery('My dog', '1920x1080', new Date());
 
-const testDimension1 = "1920x1080"
-const testDimension2 = "1080x720"
+const testDimension1 = '1920x1080';
+const testDimension2 = '1080x720';
 
-myDogPhoto.info()
-/* Фото "My dog" имеет разрешение 1920x1080 */
+myDogPhoto.info();
+/* Фото "My dog" має дозвіл 1920x1080 */
 
-setTimeout(() => myDogPhoto.publishInfo(), 2000)
-/* Фото "My dog" было опубликовано 2 секунды назад */
+setTimeout(() => myDogPhoto.publishInfo(), 2000);
+/* Фото "My dog" було опубліковано 2 секунди тому */
 
-/* ВОПРОС: Почему метод "publishInfo" все еще имеет доступ 
-к параметрам функции "photosGallery" (например "date")? */
+/* ПИТАННЯ: Чому метод "publishInfo" все ще має доступ 
+до параметрів "photosGallery" (наприклад "date")? */
 
-console.log(myDogPhoto[testDimension1]) // true
-console.log(myDogPhoto[testDimension2]) // undefined
+console.log(myDogPhoto[testDimension1]); // true
+console.log(myDogPhoto[testDimension2]); // undefined
+
+console.log(Object.keys(photosGallery()));
